53e0252e055ca0d6db533e5554181f3aa9899275
brayanorozco <orozcotafur.brayan@gmail.com>
2021-08-03 00:45:48 +0200

    Variable names changed, styles added

--- apiData.js
+++ apiData.js
@@ -1,19 +1,26 @@
 const fetch = require("node-fetch");
 const url = 'https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json';
+let cardsData = [];
 
 fetch(url)
     .then(response => response.json())
-    .then(cardsData => {
+    .then(apiData => {
 
-        const {
-            title,
-            date,
-            featured_media,
-            _embedded
-        } = cardsData;
+        //apiData contains all the information that was taken from the API.
 
-        let author = _embedded.author[0].name;
+        apiData.forEach(fromApi => {
 
-        console.log(title.rendered, date, featured_media, author);
+            let cardData = {
+                title: fromApi.title.rendered,
+                date: fromApi.date,
+                featured_media: fromApi.featured_media,
+                name: fromApi._embedded.author[0].name
+            };
+
+            cardsData.push(cardData);
+        });
+        console.log(cardsData);
+
+        //cardData filters the information and pushes it to cardsData, which is the array containing all the card data.
     })
     .catch(err => console.log(err))
