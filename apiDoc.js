fbdce6e825e01df93e81c7a89641a9de9b60f9b7
brayanorozco <orozcotafur.brayan@gmail.com>
2021-08-04 09:18:15 +0200

    Code added to apiDOC.js

--- apiDoc.js
+++ apiDoc.js
@@ -14,7 +14,7 @@ const generateCards = async () => {
             };
             const card = document.createElement('div')
             card.classList.add('p-card--highlighted', 'col-4')
-            card.setAttribute('style', "min-height: 450px;")
+            card.setAttribute('style', "min-height: 400px;")
             card.innerHTML = `
                                 <span>CLOUD AND SERVER</span>
                                 <hr class="is-fixed-width">
@@ -22,18 +22,17 @@ const generateCards = async () => {
                                     <img src="${cardShape.featured_media}" alt="card img">
                                 </div>
                                 <div class="card-body">
-                                <h3 class="p-card__title p-heading--4"> <a>${cardShape.title} </a></h3>
+                                    <h3 class="p-card__title p-heading--4"> <a>${cardShape.title} </a></h3>
                                 </div>
                                 <p class="p-card__content p-heading--6">By <a>${cardShape.name}</a> on ${cardShape.date.split("T")[0]}</p>
-                                
                                 <hr class="is-fixed-width">
                                 <span>Article</span>
                         
                             `;
             cardContainer.appendChild(card);
         });
-    } catch (e) {
-        console.log("SOMETHING WENT WRONG!!!", e)
+    } catch (err) {
+        console.log("SOMETHING WENT WRONG!!!", err)
     }
 }
 
