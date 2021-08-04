27aa65560d312e3ec3507427f39fa80cee753f86
brayanorozco <orozcotafur.brayan@gmail.com>
2021-08-05 00:27:59 +0200

    Refactor to smaller functions(code)

--- apiDoc.js
+++ apiDoc.js
@@ -1,5 +1,5 @@
 const url = 'https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json';
-let cardContainer = document.querySelector('#cards-container')
+
 
 const generateCards = async () => {
 
@@ -15,41 +15,48 @@ const generateCards = async () => {
                 name: cardData._embedded.author[0].name
             };
 
-            //Formating dates using a function and the toLocaleDateString() method.
-
-            function formatDate(articleDate) {
-
-                let date = new Date(articleDate)
-                return date.toLocaleDateString('en-UK', {
-                    year: 'numeric',
-                    month: 'long',
-                    day: '2-digit'
-                });
-            }
-            //Creating the HTML elements (cards).
-
-            const card = document.createElement('div')
-            card.classList.add('p-card--highlighted', 'col-4')
-            card.setAttribute('style', "min-height: 450px;")
-            card.innerHTML = `
-                                <p>CLOUD AND SERVER</p>
-                                <hr class="is-fixed-width">
-                                <div>
-                                    <img src="${cardShape.featured_media}" alt="card img">
-                                </div>
-                                <div class="card-body">
-                                    <h3 class="p-card__title p-heading--4"> <a>${cardShape.title} </a></h3>
-                                </div>
-                                <p class="p-card__content p-heading--6">By <a>${cardShape.name}</a> on ${formatDate(cardShape.date.split("T")[0])}</p>
-                                
-                                <hr class="is-fixed-width">
-                                <p class="p-text--small">Article</p>                        
-                            `;
-            cardContainer.appendChild(card);
+            createCard(cardShape)
         });
     } catch (err) {
-        console.log("SOMETHING WENT WRONG!!!", err)
+        console.log("It wasn't possible to reach the information", err)
     }
 }
 
-generateCards()
+//Creating the HTML elements (cards).
+
+function createCard(cardShape) {
+
+    let cardContainer = document.querySelector('#cards-container')
+    const card = document.createElement('div')
+    card.classList.add('p-card--highlighted', 'col-4')
+    card.setAttribute('style', "min-height: 450px;")
+    card.innerHTML = `
+                             <p>CLOUD AND SERVER</p>
+                             <hr class="is-fixed-width">
+                             <div>
+                                 <img src="${cardShape.featured_media}" alt="card img">
+                             </div>
+                             <div class="card-body">
+                                 <h3 class="p-card__title p-heading--4"> <a>${cardShape.title} </a></h3>
+                             </div>
+                             <p class="p-card__content p-heading--6">By <a>${cardShape.name}</a> on ${formatDate(cardShape.date.split("T")[0])}</p>
+                             
+                             <hr class="is-fixed-width">
+                             <p class="p-text--small">Article</p>                        
+                         `;
+    cardContainer.appendChild(card);
+}
+
+//Formating dates using a function and the toLocaleDateString() method.
+
+function formatDate(articleDate) {
+
+    let date = new Date(articleDate)
+    return date.toLocaleDateString('en-UK', {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit'
+    });
+}
+
+generateCards();
